c1956e7c1d2b7360c7e294b4b8179c1390fdada3
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-10 12:44:43 -0700

    Add files via upload

---
+++ hanoiMain.java
@@ -0,0 +1,11 @@
+// Meghan Flaherty
+// CS 145
+/* this program creates a puzzle in which a person is trying to move disks 
+ * between three pegs with some contraints on the order that they must be placed
+ * that the code should solve
+ */ 
+
+
+public class hanoiMain {
+    
+}
